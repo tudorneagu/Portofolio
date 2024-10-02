934872c9e14af55930aca71a5dd1d110dbfff588
tudorneagu <neagu.t@gmail.com>
2024-10-02 22:20:34 +0200

    About - OK

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,24 +8,11 @@ function About() {
           <img className=" rounded-3xl" src="/tudor.jpg" alt="Image of Tudor" />
         </article>
         <article className="flex flex-col max-w-[620px] pr-12 gap-10 items-start ">
-          <h1 className="capitalise heading-m">My Journey</h1>
+          <h1 className="capitalise heading-m">{t("about_title")}</h1>
           <div className="text-m-regular flex flex-col gap-5 ">
-            <p>
-              After a rewarding career in architecture, I transitioned into
-              full-stack web development, blending my design and problem-solving
-              skills with a passion for technology. My background in
-              architecture allows me to approach challenges with creativity and
-              structure, applying the same attention to detail and strategic
-              thinking to digital solutions.
-            </p>
+            <p>{t("about_description_1")}</p>
 
-            <p>
-              By combining these strengths with technical expertise, I build
-              scalable, user-friendly applications. My experience managing large
-              projects has honed my ability to deliver results that are both
-              practical and innovative, ensuring efficient solutions that meet
-              user needs while exceeding expectations.
-            </p>
+            <p>{t("about_description_2")}</p>
           </div>
         </article>
       </div>
