66b14dc8103da3bb57f5cddc2f988a8e63f085aa
tudorneagu <neagu.t@gmail.com>
2024-10-03 09:19:51 +0200

    translation correction

--- src/components/Projects/ProjectsPreview.tsx
+++ src/components/Projects/ProjectsPreview.tsx
@@ -2,7 +2,9 @@ import Button from "../ui/Buttons/Button";
 import ProjectCard from "./ProjectCard";
 import ProjectData from "../../data/projects.json";
 import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 function ProjectsPreview() {
+  const { t } = useTranslation();
   return (
     <section className="flex flex-col gap-14 items-center  py-10 px-20  bg-bg-light  ">
       <article className="flex gap-4 ">
@@ -16,6 +18,8 @@ function ProjectsPreview() {
           />
         ))}
       </article>
+      <p> {t("under_construction_1")}</p>
+
       <NavLink to={"/projects"}>
         <Button content="See my work" />
       </NavLink>
--- src/pages/Stack.tsx
+++ src/pages/Stack.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 function Stack() {
+  const { t } = useTranslation();
+
   return (
     <div className="text-l-bold items-center w-full justify-center flex">
       {" "}
-      Page under Construction
+      {t("under_construction")}
     </div>
   );
 }
