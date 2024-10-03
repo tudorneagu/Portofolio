d65c781b960bf501bf5d0fd14657e3ebc8dfcc88
tudorneagu <neagu.t@gmail.com>
2024-10-03 09:03:09 +0200

    translation correction

--- src/components/ContactPreview.tsx
+++ src/components/ContactPreview.tsx
@@ -1,14 +1,18 @@
+import { useTranslation } from "react-i18next";
 import Button from "./ui/Buttons/Button";
 import phoneNumberFormat from "./utils/PhoneNumber.ts";
 import { NavLink } from "react-router-dom";
 function ContactPreview() {
+  const { t } = useTranslation();
   return (
     <section className=" items-center flex flex-col gap-5 justify-center  bg-bg-dark px-20 py-10">
-      <h1 className="heading-m"> Let's Work Together !</h1>
+      <h1 className="heading-m"> {t("hero_contact_title")}</h1>
       <NavLink to={`/contact`}>
-        <Button content="Send me a Message" />
+        <Button content={t("hero_contact_button")} />
       </NavLink>
-      <p> Phone: {phoneNumberFormat("660388641")}</p>
+      <p>
+        {t("hero_contact_phone")} {phoneNumberFormat("660388641")}
+      </p>
     </section>
   );
 }
