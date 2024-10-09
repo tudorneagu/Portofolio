1ce15faf2372c6e4f17f0a82da498d2ee495e1bd
tudorneagu <neagu.t@gmail.com>
2024-10-09 10:57:54 +0200

    carrousel image size updated

--- src/components/ui/Caroussel.tsx
+++ src/components/ui/Caroussel.tsx
@@ -9,23 +9,23 @@ function Caroussel({ images, title }: { images: string[]; title: string }) {
 
   return (
     <div className="relative max-w-2xl mx-auto">
-      <div className="w-full h-30 overflow-hidden">
+      <div className="w-full h-48 overflow-hidden">
         {images.map((image: string, index: number) => (
           <img
             key={index}
             src={imagesPath[index]}
             alt={`carousel-${image}`}
-            className={`w-full   transition-opacity duration-500 ease-in-out ${
+            className={`w-auto h-full object-contain transition-opacity duration-500 ease-in-out ${
               index === currentIndex ? "block" : "hidden"
             }`}
           />
         ))}
       </div>
-      <div className="flex justify-center items-center space-x-2 mt-4">
+      <div className="w-full  flex justify-center items-center space-x-2 mt-4">
         {imagesPath.map((_, index) => (
           <span
             key={index}
-            className={`rounded-full cursor-pointer ${
+            className={`rounded-full  cursor-pointer ${
               index === currentIndex
                 ? "bg-black h-3 w-3"
                 : "bg-bg-light h-2 w-2 "
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ function About() {
   const { t } = useTranslation();
   return (
     <section className="w-full flex  justify-around items-center  py-5 px-10 lg:px-20  bg-bg-light ">
-      <div className="bg-bg-dark rounded-lg flex flex-col lg:flex-rowgap-114 p-10 w-full justify-around items-center">
+      <div className="bg-bg-dark rounded-lg flex flex-col lg:flex-row gap-114 p-10 w-full justify-around items-center">
         <article className="max-w-80 max-h-auto my-5 lg:my-10">
           <img className=" rounded-3xl" src="/tudor.jpg" alt="Image of Tudor" />
         </article>
