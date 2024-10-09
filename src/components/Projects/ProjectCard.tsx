d5343409e70ebb76070f447f98c060db7c7e184d
tudorneagu <neagu.t@gmail.com>
2024-10-09 11:20:16 +0200

    added project ressources

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import Button from "../ui/Buttons/Button";
-import Caroussel from "../ui/caroussel";
+import Caroussel from "../ui/Caroussel";
 import type { Project } from "../../@types/index.types";
 
 function ProjectCard({ date, title, description, link, images }: Project) {
