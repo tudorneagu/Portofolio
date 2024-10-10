94388efabe0bd90d5451d9c13c5873805f6224d6
tudorneagu <neagu.t@gmail.com>
2024-10-10 09:25:20 +0200

    bug fix on link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import Language from "./Language";
 import Logo from "../Logo";
 import Nav from "./Nav";
@@ -11,10 +13,11 @@ function Header({
 }) {
   return (
     <header className="flex  justify-between  items-center py-5 lg:py-5 px-10 lg:px-20  bg-bg-light z-30 ">
-      <div className="flex items-center gap-1 lg:heading-s">
+      <Link to={`/`} className="flex items-center gap-1 lg:heading-s">
         <Logo />
+
         <p className=" text-l-regular animate-blink">|</p>
-      </div>
+      </Link>
       <Nav langFr={langFr} setLangFr={setLangFr} />
       <div className="hidden lg:block">
         <Language langFr={langFr} setLangFr={setLangFr} />
